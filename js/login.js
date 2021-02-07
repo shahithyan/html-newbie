ba8eeaa78c14e60fb697d406a146075db0893aca
shahithyan <shahithyan2010@gmail.com>
2021-02-07 11:09:35 -0600

    login successful

--- js/login.js
+++ js/login.js
@@ -4,12 +4,9 @@ function login(){
     
     if(userName !== "pokemon"){
         document.getElementById("loginFailure").style.display = "block";
-        document.getElementById("loginSuccessful").style.display = "none";
     } else if(password !== "Mewtwo"){
         document.getElementById("loginFailure").style.display = "block";
-        document.getElementById("loginSuccessful").style.display = "none";
     } else {
-        document.getElementById("loginFailure").style.display = "none";
-       document.getElementById("loginSuccessful").style.display = "block";
+       window.location.href = "./../forms/loginsuccesful.html"
     }
 } 
