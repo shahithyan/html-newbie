e1f4972ea639e92bff5020dc323b9fd6427292b0
shahithyan <shahithyan2010@gmail.com>
2021-03-23 15:50:39 -0500

    calling a function using enter key

--- js/button.js
+++ js/button.js
@@ -1,6 +1,6 @@
 function myFunction() {
     var buttonText = document.getElementById("myButton").innerHTML;
-
+    
     if(buttonText === "Button is clicked"){
         document.getElementById("myButton").innerHTML = "I am a button";
     } else{
